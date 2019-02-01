380e6b66347f3973be3a61996ee386d3696162de
charlespv <charlespv@hotmail.fr>
2019-02-01 16:03:42 +0100

    init ml package

---
+++ ml/train.py
@@ -0,0 +1,44 @@
+"""
+The :mod:`ml.train` module implements utilities
+to train a model
+@param {List < String >} features
+@param { String } filepath
+@return { Void } Save model
+@return { string } file path
+
+- Fit
+- Crossvalidation
+"""
+# Author: Charles Paulas Victor
+
+import pandas as pd
+
+def open_dataset(filepath):
+  """
+  function open_dataset: 
+
+  Parameters
+  ----------
+  filepath: string
+  File path of the clean dataset
+
+  Return 
+  -------
+  df_res : pd.DataFrame
+  Return a dataframe ready to use for training
+
+  """
+  df_res = pd.read_csv(filepath)
+
+  return df_res
+
+# TEST
+
+PATH_TO_FILE = "data/dataset.csv"
+
+open_dataset(PATH_TO_FILE).head(5)
+
+print(open_data)
+
+
+# END TEST
