b970d18bc47f88a92311835b11324c17d13ed255
Brendan <brendan.poirier@hetic.net>
2019-02-01 16:03:33 +0100

    Configure RunForest first views

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/train/upload')
+def train_upload():
+    
+    return render_template('train/upload.html')
+
+@app.route('/train/configure')
+def train_configure():
+    return render_template('train/configure.html')
+
+# Files to watch
--- run.py
+++ run.py
@@ -8,17 +8,17 @@ def run():
     print('Running app on ' + os_name)
 
     if os_name == "Windows":
-        subprocess.call("set FLASK_APP=app.py & python -m flask run", shell=True)
+        subprocess.call("set FLASK_APP=app.py & set FLASK_DEBUG=1 & python -m flask run", shell=True)
     elif os_name == "Darwin":
-        subprocess.call("export FLASK_APP=app.py && python -m flask run", shell=True)
+        subprocess.call("export FLASK_APP=app.py && export FLASK_DEBUG=1 && python -m flask run", shell=True)
     else:
         print('Your OS is not compatible...')
 
 # Manage command exit
-try:
-    if __name__ == '__main__':
+if __name__ == '__main__':
+    try:
         run()
-except KeyboardInterrupt:
-    # quit
-    sys.exit()
+    except KeyboardInterrupt:
+        # quit
+        sys.exit()
 
